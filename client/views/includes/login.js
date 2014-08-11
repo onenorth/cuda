799e58ed774c434f26e88dbf2c789b99e05fd3e8
askesian <cwigley@gmail.com>
2014-08-11 10:58:09 -0500

    Start login process

--- client/views/includes/login.js
+++ client/views/includes/login.js
@@ -1,10 +1,19 @@
-// Meteor.loginAsAdmin = function(password, callback) {
-//   //create a login request with admin: true, so our loginHandler can handle this request
-//   var loginRequest = {admin: true, password: password};
+Template.login.events({
+  'click .js-btn-login': function() {
+    var uname = $('.js-login-uname').val(),
+        pwd = $('.js-login-password').val();
+    //Notifications.update(this._id, {$set: {read: true}});
+    Meteor.loginAsAdmin(uname, pwd);
+  }
+})
 
-//   //send the login request
-//   Accounts.callLoginMethod({
-//     methodArguments: [loginRequest],
-//     userCallback: callback
-//   });
-// };
+Meteor.loginAsAdmin = function(uname, password, callback) {
+  //create a login request with admin: true, so our loginHandler can handle this request
+  var loginRequest = {admin: true, user: uname, password: password};
+
+  //send the login request
+  Accounts.callLoginMethod({
+    methodArguments: [loginRequest],
+    userCallback: callback
+  });
+};
