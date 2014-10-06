d37b053acccf617d85aa771be6d07937fa80097b
askesian <cwigley@gmail.com>
2014-10-06 11:16:49 -0500

    use update rule to control when updates occur

--- client/views/rules/rule_edit.js
+++ client/views/rules/rule_edit.js
@@ -19,6 +19,8 @@ Template.ruleEdit.events({
     e.preventDefault();
 
     var currRuleId = this._id;
+    var doc = ContentRules.find({_id: currRuleId });
+
     var form = tmpl.find('form');
     var fields = parseForm(form);
 
@@ -37,20 +39,23 @@ Template.ruleEdit.events({
     //     comments: $(e.target).find('[name=comments]').val()
     //   }
     // };
+    //
+    ContentRules.update(currRuleId, {$set: fields});
+    Router.go('home');
 
-    Meteor.call('updateRule', fields, function(error) {
-      if( error ) {
-        throwError(error.reason);
-        if( error.error === 302 ) {
-          Router.go( 'home', {_id: error.details} );
-        } else {
-          Router.go('home');
-        }
-      } else {
-        ContentRules.update(currRuleId, {$set: fields});
-        Router.go('home');
-      }
-    });
+    // Meteor.call('updateRule', fields, function(error) {
+    //   if( error ) {
+    //     throwError(error.reason);
+    //     if( error.error === 302 ) {
+    //       Router.go( 'home', {_id: error.details} );
+    //     } else {
+    //       Router.go('home');
+    //     }
+    //   } else {
+    //     ContentRules.update(currRuleId, {$set: fields});
+    //     Router.go('home');
+    //   }
+    // });
 
   },
 
